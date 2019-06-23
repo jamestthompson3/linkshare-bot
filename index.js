b6b8f8910ec41c5170e5563875c6379b54c67bd4
Taylor Thompson <jamestthompson3@gmail.com>
2019-06-23 19:02:28 +0300

    Adds command for getting chat id

--- index.js
+++ index.js
@@ -6,28 +6,36 @@ const TELEGRAM_URL = `https://api.telegram.org/bot${
   process.env.TELEGRAM_KEY
 }/sendMessage`;
 
+function postToBot(res, chatId, text) {
+  axios
+    .post(TELEGRAM_URL, {
+      chat_id: chatId,
+      text
+    })
+    .then(() => res.end("ok"))
+    .catch(err => res.end(`Error: ${err}`));
+}
+
 module.exports = async (req, res) => {
   const body = await json(req);
   const { message } = body;
   const urlRegex = /((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=\+\$,\w]+@)?[A-Za-z0-9.-]+|(?:www.|[-;:&=\+\$,\w]+@)[A-Za-z0-9.-]+)((?:\/[\+~%\/.\w-_]*)?\??(?:[-\+=&;%@.\w_]*)#?(?:[\w]*))?)/;
-
-  if (message.entities) {
-    console.log(message.entities);
-    const command = message.entities.find(
-      entity => entity.type === "bot_command"
-    );
-    console.log(command);
-  }
-
-  if (!message || !urlRegex.test(message.text)) {
+  if (!message) {
     return res.end();
   }
 
-  axios
-    .post(TELEGRAM_URL, {
-      chat_id: message.chat.id,
-      text: message.text
-    })
-    .then(() => res.end("ok"))
-    .catch(err => res.end(`Error: ${err}`));
+  if (message.entities) {
+    const command = message.entities.find(
+      entity => entity.type === "bot_command"
+    );
+    if (command && message.text.includes("/id")) {
+      return postToBot(res, message.chat.id, message.chat.id);
+    }
+  }
+
+  if (!urlRegex.test(message.text)) {
+    return res.end();
+  }
+
+  postToBot(res, message.chat.id, message.text);
 };
